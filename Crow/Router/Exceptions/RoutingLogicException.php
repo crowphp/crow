bff67deb74fef3e07e275bb2f853f6dd719a84b3
douglasz1 <douglas.santana.dev@gmail.com>
2021-07-21 19:40:12 -0300

    giving a relevant info

--- Crow/Router/Exceptions/RoutingLogicException.php
+++ Crow/Router/Exceptions/RoutingLogicException.php
@@ -11,6 +11,7 @@ class RoutingLogicException extends LogicException
 {
     public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
     {
+        //construct is definng a method parent of these variables
         parent::__construct($message, $code, $previous);
     }
 }
